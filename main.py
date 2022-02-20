f56e9aaf532956346772decb5644ff46e8af8fd2
tuguzT <timurka.tugushev@gmail.com>
2022-02-20 13:22:01 +0300

    Execute queries

--- main.py
+++ main.py
@@ -12,5 +12,41 @@ def create_session() -> Session:
     return session
 
 
+# noinspection PyShadowingNames
+def get_users(session: Session) -> list:
+    result = session.execute("SELECT * FROM users")
+    return [x for x in result]
+
+
+# noinspection PyShadowingNames
+def execute_queries(session: Session):
+    session.execute("TRUNCATE TABLE users")
+
+    result = get_users(session)
+    print(result)
+
+    session.execute("INSERT INTO users (id, name, login, group) VALUES (1, 'User', 'setevoy', 'wheel')")
+    result = get_users(session)[0]
+    print(result)
+
+    result = get_users(session)[0]
+    print(result.login, result.name)
+
+    result = get_users(session)
+    for x in result:
+        print(x.id)
+
+    name, login, group = 'newuser', 'newlogin', 'newgroup'
+    session.execute('INSERT INTO users (id, name, login, group) VALUES (2, %s, %s, %s)', (name, login, group))
+    result = get_users(session)
+    print(result)
+
+    _dict = {'name': 'secondname', 'login': 'secondlogin', 'group': 'secondgroup', }
+    session.execute('INSERT INTO users (id, name, login, group) VALUES (2, %(name)s, %(login)s, %(group)s)', _dict)
+    result = get_users(session)
+    print(result)
+
+
 if __name__ == '__main__':
     session: t.Final[Session] = create_session()
+    execute_queries(session)
