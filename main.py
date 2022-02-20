bef07400d4b4914eeb83f70652c1c6ca10f13200
tuguzT <timurka.tugushev@gmail.com>
2022-02-20 12:43:28 +0300

    Create session

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
+from cassandra.cluster import Cluster, Session
+import typing as t
+
+
+keyspace: t.Final[str] = 'TestKeyspace'.lower()
+
+
+# noinspection PyShadowingNames
+def create_session() -> Session:
+    cluster = Cluster()
+    session = cluster.connect(keyspace)
+    return session
+
+
 if __name__ == '__main__':
-    print('Hello World!')
+    session: t.Final[Session] = create_session()
